5c0d9a7ba8fa85c2f5579959430ef8ea85f6b21c
gunjanjaju <gunjanjajubkn2gmail.com>
2020-07-11 14:35:57 +0530

    giikk

--- app/src/main/java/com/example/cliqbuzz/GroupFragment.java
+++ app/src/main/java/com/example/cliqbuzz/GroupFragment.java
@@ -1,12 +1,29 @@
 package com.example.cliqbuzz;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.telephony.mbms.MbmsErrors;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +31,14 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class GroupFragment extends Fragment {
+
+    private View groupFragmentView;
+    private ListView list_View;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_groups = new ArrayList<>();
+    private DatabaseReference GroupRef;
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,7 +50,7 @@ public class GroupFragment extends Fragment {
     private String mParam2;
 
     public GroupFragment() {
-        // Required empty public constructor
+
     }
 
     /**
@@ -57,8 +82,66 @@ public class GroupFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group, container, false);
+                             Bundle savedInstanceState)
+    {
+
+        groupFragmentView = inflater.inflate(R.layout.fragment_group, container, false);
+
+
+        GroupRef = FirebaseDatabase.getInstance().getReference().child("Groups");
+
+        InitializeFields();
+
+        RetrieveAndDisplayGroups();
+
+
+        list_View.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                String currentGroupName = (String) adapterView.getItemAtPosition(position);
+                Intent groupChatIntent = new Intent(getContext(), GroupChatActivity.class);
+                groupChatIntent.putExtra( "groupName", currentGroupName);
+                startActivity(groupChatIntent);
+            }
+        });
+
+        return  groupFragmentView;
+    }
+
+
+
+    private void InitializeFields()
+    {
+        list_View = (ListView) groupFragmentView.findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, list_of_groups);
+        list_View.setAdapter(arrayAdapter);
+    }
+
+
+    private void RetrieveAndDisplayGroups()
+    {
+      GroupRef.addValueEventListener(new ValueEventListener() {
+          @Override
+          public void onDataChange(@NonNull DataSnapshot datasnapshot)
+          {
+              Set<String> set = new HashSet<>();
+              Iterator iterator = datasnapshot.getChildren().iterator();
+
+              while (iterator.hasNext())
+              {
+                  set.add(((DataSnapshot)iterator.next()).getKey());
+              }
+
+              list_of_groups.clear();;
+              list_of_groups.addAll(set);
+              arrayAdapter.notifyDataSetChanged();
+          }
+
+          @Override
+          public void onCancelled(@NonNull DatabaseError error) {
+
+          }
+      });
     }
 }
--- app/src/main/java/com/example/cliqbuzz/MainActivity.java
+++ app/src/main/java/com/example/cliqbuzz/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.cliqbuzz;
 
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
@@ -24,6 +40,7 @@ public class MainActivity extends AppCompatActivity
     private int CliqBuzz;
     private FirebaseUser currentUser;
     private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
 
     public MainActivity(int cliqBuzz, FirebaseUser currentUser) {
         CliqBuzz = cliqBuzz;
@@ -40,6 +57,7 @@ public class MainActivity extends AppCompatActivity
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+        RootRef = FirebaseDatabase.getInstance().getReference();
         
         Toolbar mToolbar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -73,13 +91,137 @@ public class MainActivity extends AppCompatActivity
         {
             SendUserToLoginActivity();
         }
+        else
+        {
+            VerifyUserExistance();
+        }
+    }
+
+    private void VerifyUserExistance()
+    {
+        String currentuserID = mAuth.getCurrentUser().getUid();
+        RootRef.child("Users").child(currentuserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+               if ((dataSnapshot.child("name").exists()))
+               {
+                   Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+               }
+               else
+               {
+                  SendUserToSettingsActivity();
+               }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.options_menu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+         super.onOptionsItemSelected(item);
+         if(item.getItemId() == R.id.main_logout_option)
+         {
+             mAuth.signOut();
+             SendUserToLoginActivity();
+         }
+
+        if(item.getItemId() == R.id.main_settings_option)
+        {
+            SendUserToSettingsActivity();
+
+        }
+        if(item.getItemId() == R.id.main_create_group_option)
+        {
+            RequestNewGroup();
+        }
+        if(item.getItemId() == R.id.main_find_friends_option)
+        {
+
+        }
+        return true;
+    }
+
+    private void RequestNewGroup()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this,R.style.AlertDialog);
+        builder.setTitle("Enter Group Name :");
+        final EditText groupNameField = new EditText(MainActivity.this);
+        groupNameField.setHint("Group name");
+        builder.setView(groupNameField);
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+              String groupName = groupNameField.getText().toString();
+              if (TextUtils.isEmpty(groupName))
+              {
+                  Toast.makeText(MainActivity.this, "Please write Group Name", Toast.LENGTH_SHORT).show();
+              }
+              else
+              {
+                  CreateNewGroup(groupName);
+              }
+            }
+        });
+
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                dialogInterface.cancel();
+
+            }
+        });
+        builder.show();
+    }
+
+    private void CreateNewGroup(final String groupName)
+    {
+        RootRef.child("Groups").child(groupName).setValue("")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                     if (task.isSuccessful())
+                     {
+                         Toast.makeText(MainActivity.this, groupName + "group is Created Sucessfully....", Toast.LENGTH_SHORT).show();
+                     }
+                    }
+                });
     }
 
     private void SendUserToLoginActivity()
     {
         Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
+        finish();
     }
-
-
+    private void SendUserToSettingsActivity()
+    {
+        Intent settingsIntent = new Intent(MainActivity.this,SettingsActivity.class);
+        settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(settingsIntent);
+        finish();
+    }
 }
